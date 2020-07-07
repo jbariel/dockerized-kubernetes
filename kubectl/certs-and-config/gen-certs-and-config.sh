f09e070b4bfeeacd3f7f8511d3b603fcfd26ec33
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-07-07 21:58:48 +0000

    rename folder to be more sensible, add encryption support, full config support, fix issues to make it run, now runs and builds

--- kubectl/certs-and-config/gen-certs-and-config.sh
+++ kubectl/certs-and-config/gen-certs-and-config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -25,27 +25,17 @@ K8_API_SERVER_IP=172.18.6.130
 #  $1.kubeconfig
 function genCsrAndCertAndConfig
 {
+    echo "Generating ${1}..."
+    echo -e "\t${1}\n\t${2}\n\t${3}\n\t${4}"
+
     sed "s/CERTNAME_HERE/${2}/g" generic-csr.json > ${1}-csr.json
     sed -i "s/CERTORG_HERE/${3}/g" ${1}-csr.json
     cfssl gencert ${4} ${1}-csr.json | cfssljson -bare ${1}
 
-    kubectl config set-cluster dockerized-kubernetes \
-        --certificate-authority=ca.pem \
-        --embed-certs=true \
-        --server=https://${K8_API_SERVER_IP}:6443 \
-        --kubeconfig=${1}.kubeconfig
-
-    kubectl config set-credentials ${2} \
-        --client-certificate=${1}.pem \
-        --client-key=${1}-key.pem \
-        --embed-certs=true \
-        --kubeconfig=${1}.kubeconfig
-
-    kubectl config set-context default \
-        --cluster=dockerized-kubernetes \
-        --user=${2} \
-        --kubeconfig=${1}.kubeconfig
-
+    ## All the fun of k8 configuration based on certs
+    kubectl config set-cluster dockerized-kubernetes --certificate-authority=ca.pem --embed-certs=true --server=https://${K8_API_SERVER_IP}:6443 --kubeconfig=${1}.kubeconfig
+    kubectl config set-credentials ${2} --client-certificate=${1}.pem --client-key=${1}-key.pem --embed-certs=true --kubeconfig=${1}.kubeconfig
+    kubectl config set-context default --cluster=dockerized-kubernetes --user=${2} --kubeconfig=${1}.kubeconfig
     kubectl config use-context default --kubeconfig=${1}.kubeconfig
 }
 
@@ -54,33 +44,36 @@ genCsrAndCertAndConfig 'admin' 'admin' 'system:masters' "${BASE_CERT_PARAMS}"
 
 # Create the client cert(s)
 for I in {0..2}; do
-    genCsrAndCertAndConfig
-
-
- "kublet-${I}" "system:node:kublet_${I}" 'system:nodes' "${BASE_CERT_PARAMS} -hostname=kublet_${I},172.18.6.18${I}"
+    genCsrAndCertAndConfig "kublet-${I}" "system:node:kublet_${I}" 'system:nodes' "${BASE_CERT_PARAMS} -hostname=kublet_${I},172.18.6.18${I}"
 done
 
 # Create the kube controller manager client cert
 genCsrAndCertAndConfig 'kube-controller-manager' 'system:kube-controller-manager' 'system:kube-controller-manager' "${BASE_CERT_PARAMS} -hostname=172.18.6.100,kube-controller-manager"
 
-
-
 # Create the kube proxy client cert
 genCsrAndCertAndConfig 'kube-proxy' 'system:kube-proxy' 'system:node-proxier' "${BASE_CERT_PARAMS}"
-
-
 
 # Create the kube scheduler client cert
 genCsrAndCertAndConfig 'kube-scheduler' 'system:kube-scheduler' 'system:kube-scheduler' "${BASE_CERT_PARAMS} -hostname=172.18.6.120,kube-scheduler"
 
-
-
 # Create the kube api server cert
 genCsrAndCertAndConfig 'kubernetes' 'kubernetes' 'Kubernetes' "${BASE_CERT_PARAMS} -hostname=${K8_API_SERVER_IP},kube-api-server"
-
-
 
 # Create the kube service accounts cert
 genCsrAndCertAndConfig 'service-account' 'service-accounts' 'Kubernetes Service Accounts' "${BASE_CERT_PARAMS}"
 
-
+# Encryption key for data at rest...
+ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+cat > encryption-config.yaml <<EOF
+kind: EncryptionConfig
+apiVersion: v1
+resources:
+  - resources:
+      - secrets
+    providers:
+      - aescbc:
+          keys:
+            - name: key1
+              secret: ${ENCRYPTION_KEY}
+      - identity: {}
+EOF
